7909cc7789445fbe8f477a2ff37ce5a577852ec8
ajah suleiman <ajahsuleiman@gmail.com>
2024-08-22 16:35:11 +0100

    create balance component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add CaseSensitivePathsPlugin to Webpack
+    config.plugins.push(new CaseSensitivePathsPlugin());
+
+    // Other customizations if needed
+    return config;
+  },
+};
 
 export default nextConfig;
+
